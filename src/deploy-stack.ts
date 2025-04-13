45661f17735c24f002d6271ed84f17e90182e633
Kevin Lam Seck <kevin.lamseck@gmail.com>
2025-04-13 17:58:44 +0200

    init

---
+++ src/deploy-stack.ts
@@ -0,0 +1,133 @@
+import * as core from '@actions/core'
+import fs from 'fs'
+import path from 'path'
+import { PortainerApi } from './api.js'
+import Handlebars from 'handlebars'
+
+type DeployStack = {
+  portainerHost: string
+  username: string
+  password: string
+  swarmId?: string
+  endpointId: number
+  stackName: string
+  stackDefinitionFile?: string
+  templateVariables?: object
+  image?: string
+  pruneStack?: boolean
+  pullImage?: boolean
+}
+
+enum StackType {
+  SWARM = 1,
+  COMPOSE = 2,
+}
+
+function generateNewStackDefinition(
+  stackDefinitionFile?: string,
+  templateVariables?: object,
+  image?: string,
+): string | undefined {
+  if (!stackDefinitionFile) {
+    core.info(`No stack definition file provided. Will not update stack definition.`)
+    return undefined
+  }
+
+  const stackDefFilePath = path.join(process.env.GITHUB_WORKSPACE as string, stackDefinitionFile)
+  core.info(`Reading stack definition file from ${stackDefFilePath}`)
+  let stackDefinition = fs.readFileSync(stackDefFilePath, 'utf8')
+  if (!stackDefinition) {
+    throw new Error(`Could not find stack-definition file: ${stackDefFilePath}`)
+  }
+
+  if (templateVariables) {
+    core.info(`Applying template variables for keys: ${Object.keys(templateVariables)}`)
+    stackDefinition = Handlebars.compile(stackDefinition)(templateVariables)
+  }
+
+  if (!image) {
+    core.info(`No new image provided. Will use image in stack definition.`)
+    return stackDefinition
+  }
+
+  const imageWithoutTag = image.substring(0, image.indexOf(':'))
+  core.info(`Inserting image ${image} into the stack definition`)
+  return stackDefinition.replace(new RegExp(`${imageWithoutTag}(:.*)?\n`), `${image}\n`)
+}
+
+export async function deployStack({
+  portainerHost,
+  username,
+  password,
+  swarmId,
+  endpointId,
+  stackName,
+  stackDefinitionFile,
+  templateVariables,
+  image,
+  pruneStack,
+  pullImage,
+}: DeployStack): Promise<void> {
+  const portainerApi = new PortainerApi(portainerHost)
+
+  const stackDefinitionToDeploy = generateNewStackDefinition(
+    stackDefinitionFile,
+    templateVariables,
+    image,
+  )
+  if (stackDefinitionToDeploy) core.debug(stackDefinitionToDeploy)
+
+  core.info('Logging in to Portainer instance...')
+  await portainerApi.login({
+    username,
+    password,
+  })
+
+  try {
+    const allStacks = await portainerApi.getStacks()
+    const existingStack = allStacks.find((s) => s.Name === stackName)
+
+    if (existingStack) {
+      core.info(`Found existing stack with name: ${stackName}`)
+      core.info('Updating existing stack...')
+      await portainerApi.updateStack(
+        existingStack.Id,
+        {
+          endpointId: existingStack.EndpointId,
+        },
+        {
+          env: existingStack.Env,
+          stackFileContent: stackDefinitionToDeploy,
+          prune: pruneStack ?? false,
+          pullImage: pullImage ?? false,
+        },
+      )
+      core.info('Successfully updated existing stack')
+    } else {
+      if (!stackDefinitionToDeploy) {
+        throw new Error(
+          `Stack with name ${stackName} does not exist and no stack definition file was provided.`,
+        )
+      }
+      core.info('Deploying new stack...')
+      await portainerApi.createStack(
+        {
+          type: swarmId ? StackType.SWARM : StackType.COMPOSE,
+          method: 'string',
+          endpointId,
+        },
+        {
+          name: stackName,
+          stackFileContent: stackDefinitionToDeploy,
+          swarmID: swarmId ? swarmId : undefined,
+        },
+      )
+      core.info(`Successfully created new stack with name: ${stackName}`)
+    }
+  } catch (error) {
+    core.info('⛔️ Something went wrong during deployment!')
+    throw error
+  } finally {
+    core.info(`✅ Done`)
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,73 @@
+import * as core from '@actions/core'
+import { deployStack } from './deploy-stack.js'
+import axios from 'axios'
+
+/**
+ * The main function for the action.
+ *
+ * @returns Resolves when the action is complete.
+ */
+export async function run(): Promise<void> {
+  try {
+    const portainerHost: string = core.getInput('portainer-host', {
+      required: true,
+    })
+    const username: string = core.getInput('username', {
+      required: true,
+    })
+    const password: string = core.getInput('password', {
+      required: true,
+    })
+    const swarmId: string = core.getInput('swarm-id', {
+      required: false,
+    })
+    const endpointId: string = core.getInput('endpoint-id', {
+      required: false,
+    })
+    const stackName: string = core.getInput('stack-name', {
+      required: true,
+    })
+    const stackDefinitionFile: string = core.getInput('stack-definition', {
+      required: false,
+    })
+    const templateVariables: string = core.getInput('template-variables', {
+      required: false,
+    })
+    const image: string = core.getInput('image', {
+      required: false,
+    })
+    const pruneStack: boolean = core.getBooleanInput('prune-stack', {
+      required: false,
+    })
+    const pullImage: boolean = core.getBooleanInput('pull-image', {
+      required: false,
+    })
+
+    await deployStack({
+      portainerHost,
+      username,
+      password,
+      swarmId,
+      endpointId: parseInt(endpointId) || 1,
+      stackName,
+      stackDefinitionFile: stackDefinitionFile ?? undefined,
+      templateVariables: templateVariables ? JSON.parse(templateVariables) : undefined,
+      image,
+      pruneStack: pruneStack || false,
+      pullImage: pullImage || false,
+    })
+    core.info('✅ Deployment done')
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response) {
+      const {
+        status,
+        data,
+        config: { url, method },
+      } = error.response
+      return core.setFailed(
+        `AxiosError HTTP Status ${status} (${method} ${url}): ${JSON.stringify(data, null, 2)}`,
+      )
+    }
+    return core.setFailed(error as Error)
+  }
+}
